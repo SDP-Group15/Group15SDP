64cc2d8967bcb53822f6b8d6cd3ec24038877682
johnterra4m <78626757+johnterra4m@users.noreply.github.com>
2023-10-20 13:07:14 -0400

    Update BackendDemo.py

--- BackendDemo.py
+++ BackendDemo.py
@@ -1,35 +1,86 @@
 from psycopg2 import connect
-#pip install psycopg2
 import os
+import csv
 
-def openConnection(dbName: str)->type(connect()):
+
+def openConnection(dbName: str = None) -> type(connect()):
     conn = connect(
         host="localhost",
         port=5432,
-        database = dbName if dbName!=None else os.environ.get('USER')
+        database = dbName if dbName else os.environ.get('USER')
     )
     return conn
 
-def searchByGene(gene: str, connection: type(connect()))->str:
+
+def searchByGene(gene: str, connection: type(connect())) -> list:
     cursor = connection.cursor() 
+
     cursor.execute(
         f"SELECT * FROM GENE WHERE GeneID = '{gene}';")
     output = cursor.fetchall() 
-    return output
+
+    return list(output)
+
+
+def searchByMesh(mesh: str, connection: type(connect())) -> list:
+    cursor = connection.cursor() 
+
+    cursor.execute(
+        f"SELECT * FROM GENE WHERE MeSH = '{mesh}';")
+    output = cursor.fetchall() 
+
+    return list(output)
+
+
+def multipleByGeneId(geneList: list, connection: type(connect())) ->  list:
+    cursor = connection.cursor()
+    
+    for gene in geneList:
+        cursor.execute(
+            f"SELECT * FROM GENE WHERE GeneID = '{gene}';")
+        
+    return cursor.fetchall()
+
+
+def multipleByGeneMesh(meshList: list, connection: type(connect())) ->  list:
+    cursor = connection.cursor()
+    
+    for mesh in meshList:
+        cursor.execute(
+            f"SELECT * FROM GENE WHERE MeSH = '{mesh}';")
+        
+    return cursor.fetchall()
+
+
+def writeRows(data : list, fileName : str):
+    with open(f"{fileName}","w") as f:
+        csv_out = csv.writer(f)
+        csv_out.writerow(["GeneID", "MeSH", "p_value", "Enrich", "PMIDs"])
+        csv_out.writerows(data)
+
+
+def main() -> None:
+    connection = openConnection()
+
+    print("\nDemo 1")
+
+    # userInput = input("Enter Gene to search by: ").strip()
+    # data = searchByGene(gene=userInput, connection=connection)
+
+    data = searchByGene(gene=34, connection=connection)
+    writeRows(data=data, fileName='Demo.csv')
+
+
+
+    print("\nDone!\n")
+    connection.close()
+#}
 
 
 if __name__ == "__main__":
-    conn = openConnection()
-    #if the os getuser does not work in this function replace with your correct pc username
-    #make sure to when using postgresql to use the database that is named after this username
+    main()
 
-    userInput = input('Enter GENE to search by: ').strip()
-
-    output = searchByGene(gene=userInput, connection=conn)
-    for lst in output:
-        print(lst)
-
-    conn.close()
+    #todo: combine gene/mesh search singular and multiple into one function with sql statement selectors
+    #      allow for single or multiple user inputs
 
 
- 
