99a7e0eb3c30b1641e8854b52f7fbc39b8c0eb31
John Matura <78626757+jterra02@users.noreply.github.com>
2023-10-26 15:40:24 -0400

    Update BackendDemo.py

--- BackendDemo.py
+++ BackendDemo.py
@@ -5,58 +5,69 @@ import csv
 
 def openConnection(dbName: str = None) -> type(connect()):
     conn = connect(
-        host="localhost",
-        port=5432,
-        database = dbName if dbName else os.environ.get('USER')
+            host='seniordesign.cyzdvv3sqno4.us-east-1.rds.amazonaws.com',
+            port='5432',
+            database='bi' if not dbName else dbName,
+            user='postgres',
+            password='Uconn!2024'
     )
+
     return conn
 
 
 def searchByGene(gene: str, connection: type(connect())) -> list:
     cursor = connection.cursor() 
 
+    
     cursor.execute(
-        f"SELECT * FROM GENE WHERE GeneID = '{gene}';")
+        f"SELECT * FROM \"GENE\" WHERE \"GeneID\" = '{gene}' ORDER BY \"p_Value\" ASC;")
     output = cursor.fetchall() 
 
+    
     return list(output)
 
 
 def searchByMesh(mesh: str, connection: type(connect())) -> list:
     cursor = connection.cursor() 
 
+    
     cursor.execute(
-        f"SELECT * FROM GENE WHERE MeSH = '{mesh}';")
+        f"SELECT * FROM \"GENE\" WHERE \"MeSH\" LIKE '{mesh}' ORDER BY \"p_Value\" ASC;") #may need to change like depending
     output = cursor.fetchall() 
 
+    
     return list(output)
 
 
 def multipleByGeneId(geneList: list, connection: type(connect())) ->  list:
     cursor = connection.cursor()
+
     
     for gene in geneList:
         cursor.execute(
-            f"SELECT * FROM GENE WHERE GeneID = '{gene}';")
-        
-    return cursor.fetchall()
+            f"SELECT * FROM \"GENE\" WHERE \"GeneID\" = '{gene}' ORDER BY \"p_Value\" ASC;")
 
-
-def multipleByGeneMesh(meshList: list, connection: type(connect())) ->  list:
-    cursor = connection.cursor()
     
-    for mesh in meshList:
-        cursor.execute(
-            f"SELECT * FROM GENE WHERE MeSH = '{mesh}';")
-        
     return cursor.fetchall()
 
 
-def writeRows(data : list, fileName : str):
+# def multipleByGeneMesh(meshList: list, connection: type(connect())) ->  list:
+#     cursor = connection.cursor()
+    
+#     for mesh in meshList:
+#         cursor.execute(
+#             f"SELECT * FROM GENE WHERE MeSH = '{mesh}' ORDER BY p_value ASC;")
+        
+#     return cursor.fetchall()
+
+
+def writeToCsvFile(data: list, fileName: str) -> None:
     with open(f"{fileName}","w") as f:
         csv_out = csv.writer(f)
         csv_out.writerow(["GeneID", "MeSH", "p_value", "Enrich", "PMIDs"])
         csv_out.writerows(data)
+
+    return
 
 
 def main() -> None:
@@ -64,12 +75,12 @@ def main() -> None:
 
     print("\nDemo 1")
 
+
     # userInput = input("Enter Gene to search by: ").strip()
     # data = searchByGene(gene=userInput, connection=connection)
-
-    data = searchByGene(gene=34, connection=connection)
-    writeRows(data=data, fileName='Demo.csv')
-
+    # data = searchByMesh(mesh='Acyl-CoA Dehydrogenase', connection=connection)   #* manual use *
+    data = searchByGene(gene=34, connection=connection)                       #* manual use *
+    writeToCsvFile(data=data, fileName='Demo.csv')
 
 
     print("\nDone!\n")
@@ -79,8 +90,4 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
-    #todo: combine gene/mesh search singular and multiple into one function with sql statement selectors
-    #      allow for single or multiple user inputs
-
 
