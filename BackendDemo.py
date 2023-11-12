c2c3206bd14067c9a907868cdaabb606c25f4a71
John Matura <78626757+jterra02@users.noreply.github.com>
2023-11-12 17:01:50 -0500

    Update BackendDemo.py
    Added listAllGene/Mesh for use in searching.

--- BackendDemo.py
+++ BackendDemo.py
@@ -3,6 +3,7 @@ import os
 import csv
 import simplejson
 import json
+import scipy
 
 
 def openConnection(dbName: str = None) -> type(connect()):
@@ -42,15 +43,33 @@ def searchByMesh(mesh: str, connection: type(connect())) -> tuple:
 def multipleByGeneId(geneList: list, connection: type(connect())) ->  tuple:
     cursor = connection.cursor()
     
+    pValIndex = 2
     output = []
+    pvals = []
     for gene in geneList:
         cursor.execute(
             f"SELECT * FROM \"GENE\" WHERE \"GeneID\" = '{gene}' ORDER BY \"p_Value\" ASC;")
         tempregister = cursor.fetchall()
-        if(tempregister): output+= tempregister
+        if(tempregister):
+            output += tempregister
+            for row in tempregister:
+                pvals.append(float(row[pValIndex]))  
+
+    print(combinePVals(pValues=pvals))
 
     return output, simplejson.dumps(output, indent=2)
 
+
+def combinePVals(pValues: list) -> float:
+    res = None
+    try:
+        res = scipy.stats.combine_pvalues(pValues, method='fisher',weights=None)
+    except:
+        print("Exception occured")
+        return None
+
+    return res[1] if res else None
+    
 
 def writeJsonToTxt(data: list, fileName: str = 'Demo.txt') -> None:
     with open(f"{fileName}","w") as f:
@@ -66,6 +85,28 @@ def writeToCsvFile(data: list, fileName: str = 'Demo.csv') -> None:
         csv_out.writerows(data)
 
     return 
+
+
+def listAllGene(connection: type(connect())) -> list:
+    cursor = connection.cursor() 
+
+    cursor.execute(
+        f"SELECT DISTINCT \"GeneID\" FROM \"GENE\" ORDER BY \"GeneID\" ASC;")
+    
+    data = cursor.fetchall()
+
+    return [str(i[0]) for i in data]
+
+
+def listAllMesh(connection: type(connect())) -> list:
+    cursor = connection.cursor() 
+
+    cursor.execute(
+        f"SELECT \"MeSH\" FROM (SELECT * FROM \"GENE\" ORDER BY \"p_Value\" ASC) AS A;")
+    
+    data = cursor.fetchall()
+    
+    return [str(i[0]) for i in data]
 
 
 def console() -> None:
@@ -88,15 +129,19 @@ def console() -> None:
 def main() -> None:
     connection = openConnection()
 
-    output = multipleByGeneId(geneList=[34,37,128], connection=connection)
+    # output = multipleByGeneId(geneList=[34,37], connection=connection)
+
+    # print(listAllGene(connection=connection))
+    print(listAllMesh(connection=connection))
+
 
     # output = searchByMesh(mesh='Acyl-CoA Dehydrogenase', connection=connection) #* manual use *
     # output = searchByGene(gene=34, connection=connection)                       #* manual use *
 
-    print(output[0])
+    # print(output[0])
 
-    writeToCsvFile(data=output[0])
-    writeJsonToTxt(data=output[1])
+    # writeToCsvFile(data=output[0])
+    # writeJsonToTxt(data=output[1])
 
 
 
