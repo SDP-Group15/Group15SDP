8ba9941bd1d990e79a4013c1e2c7c4dde7811c2d
Isaiah-Haynes <112427446+Isaiah-Haynes@users.noreply.github.com>
2023-11-13 10:59:48 -0500

    added api functionality

--- UserInterface.py
+++ UserInterface.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, jsonify, request, send_file
+from Backend import openConnection, searchByGene
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#example api run, this will return all genes with id '34' from the database
+#will add functionality to take input later --Isaiah
+@app.route('/api-test', methods = ['GET'])
+def hello():
+    dbConection = openConnection()
+    gene = searchByGene('34', dbConection)
+    return gene[1]
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
